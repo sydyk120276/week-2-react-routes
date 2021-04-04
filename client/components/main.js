2c5b9c4518057fa7b8837636aaa63ddb3c591947
sydyk120276 <akbar@github.com>
2021-04-04 19:50:36 +0600

    новый коми 4

--- client/components/main.js
+++ client/components/main.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Head from './head'
+// import Head from './head'
 
 
 const Main = () => {
   return (
     <div>
-      <Head title="Hello" />
+      {/* <Head title="Hello" /> */}
       <div id="title"> Main </div>
       <Link to="/dashboard/profile/75cbfcf2-1b69-46e1-8455-21fe7befbefa">  Profile  </Link>
       <Link to="/dashboard">  Dashboard  </Link>
